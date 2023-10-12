df69315b89f98d86bae0bd7cd70c376e28fa6974
Alessandro Angheben <aleangheben@hotmail.com>
2023-10-12 16:05:35 +0200

    non si aggiunge la classe

--- javascript/tombola.js
+++ javascript/tombola.js
@@ -3,31 +3,35 @@ const button = document.getElementById("genButton");
 
 let token = [];
 console.log(token);
-const createBox = function () {
-  for (let i = 0; i < 76; i++) {
-    const myBox = document.createElement("div");
+const createBox = function (tokens) {
+  for (let i = 0; i < tokens; i++) {
     token.push(i + 1);
-    myBox.className = "casella";
+    const myBox = document.createElement("div");
+    myBox.classList.add("casella");
     const boxNum = document.createElement("h3");
     boxNum.innerText = i + 1;
     myBox.appendChild(boxNum);
     tabellone.appendChild(myBox);
   }
-  button.onclick = function () {
-    const randomNum = Math.ceil(Math.random() * 76);
-
-    if (token.includes(randomNum)) {
-      token.classList.add("pastNum");
-    }
-  };
-  //     if (randomNum == parseInt.boxNum) {
-  //       myBox.classList.add("pastNums");
-  //     }
-  //   };
 };
+console.log(createBox(76));
+
+button.onclick = function () {
+  const randomNum = Math.ceil(Math.random() * 76);
+  for (let i = 0; i < token.length; i++) {
+    if (randomNum === token[i]) {
+      token[i].style.backgroundColor = "red";
+    }
+  }
+
+  console.log(randomNum);
+};
+//     if (randomNum == parseInt.boxNum) {
+//       myBox.classList.add("pastNums");
+//     }
+//   };
 
 // const colorBox = () => {
 //   if (randomNum === myBox.innerText) {
 //     myBox.style.backgroundColor = "red";
 //   }
-createBox();
